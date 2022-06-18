ac12d21256d5632cd7eb4911dfa14a8bf82f68d1
Alejandro Lahoz <alejandro.lahoz.dev@gmail.com>
2022-06-18 18:08:06 +0200

    Request cached by store

--- src/app/shared/store/data/data.effects.ts
+++ src/app/shared/store/data/data.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, withLatestFrom } from 'rxjs/operators';
 import { TmdbService } from '../../services/tmdb/tmdb.service';
 import {
   loadMovieDetail,
@@ -14,32 +15,45 @@ import {
   loadTVShowsError,
   loadTVShowsSuccess,
 } from './data.actions';
+import { getListMovie, getListTVShow } from './data.selectors';
 
 @Injectable()
 export class DataEffects {
-  constructor(private actions$: Actions, private tmdbService: TmdbService) {}
+  constructor(
+    private actions$: Actions,
+    private tmdbService: TmdbService,
+    private store: Store
+  ) {}
 
   loadMovies$ = createEffect(() =>
     this.actions$.pipe(
       ofType(loadMovies),
-      mergeMap(() =>
-        this.tmdbService.getPopularMovies().pipe(
+      withLatestFrom(this.store.select(getListMovie)),
+      mergeMap(([_, cache]) => {
+        if (cache.length) {
+          return of(loadMoviesSuccess({ payload: cache }));
+        }
+        return this.tmdbService.getPopularMovies().pipe(
           map((movies) => loadMoviesSuccess({ payload: movies })),
           catchError(() => of(loadMoviesError()))
-        )
-      )
+        );
+      })
     )
   );
 
   loadTVShows$ = createEffect(() =>
     this.actions$.pipe(
       ofType(loadTVShows),
-      mergeMap(() =>
-        this.tmdbService.getPopularTVShows().pipe(
+      withLatestFrom(this.store.select(getListTVShow)),
+      mergeMap(([_, cache]) => {
+        if (cache.length) {
+          return of(loadTVShowsSuccess({ payload: cache }));
+        }
+        return this.tmdbService.getPopularTVShows().pipe(
           map((tvShows) => loadTVShowsSuccess({ payload: tvShows })),
           catchError(() => of(loadTVShowsError()))
-        )
-      )
+        );
+      })
     )
   );
 
